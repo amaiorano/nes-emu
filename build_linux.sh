908ae5b71262960849bc9a4fc4dab315516a0376
Antonio Maiorano <amaiorano@gmail.com>
2016-03-11 23:05:38 -0500

    Enabled optimized builds in build_linux.sh

--- build_linux.sh
+++ build_linux.sh
@@ -2,18 +2,22 @@
 
 export SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
+export BUILD_TYPE=Release
+#export BUILD_TYPE=Debug
+
 cd $SCRIPT_DIR
 mkdir build_gcc
 cd build_gcc
 export CC=`which gcc`
 export CXX=`which g++`
-cmake ..
-make -j
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
+make -j VERBOSE=1
 
 cd $SCRIPT_DIR
 mkdir build_clang
 cd build_clang
 export CC=`which clang`
 export CXX=`which clang++`
-cmake ..
-make -j
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
+make -j VERBOSE=1
+
